58a28aff6e3fb351d781e5d9a40f6ddf933cb148
Vivian Chan <viviancpy@gmail.com>
2017-12-29 10:34:10 +1100

    add logging

--- index.js
+++ index.js
@@ -153,7 +153,37 @@ function handlePostback(sender_psid, received_postback) {
     callSendAPI(sender_psid, yesPayload);
   } else if (payload === START_SEARCH_NO) {
     const noPayload = {
-      "text": "it's ok"
+      "attachment":{
+         "type":"template",
+         "payload":{
+           "template_type":"generic",
+           "elements":[
+              {
+               "title":"Welcome to Peter'\''s Hats",
+               "image_url":"https://petersfancybrownhats.com/company_image.png",
+               "subtitle":"We'\''ve got the right hat for everyone.",
+               "default_action": {
+                 "type": "web_url",
+                 "url": "https://peterssendreceiveapp.ngrok.io/view?item=103",
+                 "messenger_extensions": true,
+                 "webview_height_ratio": "tall",
+                 "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+               },
+               "buttons":[
+                 {
+                   "type":"web_url",
+                   "url":"https://petersfancybrownhats.com",
+                   "title":"View Website"
+                 },{
+                   "type":"postback",
+                   "title":"Start Chatting",
+                   "payload":"DEVELOPER_DEFINED_PAYLOAD"
+                 }
+               ]
+             }
+           ]
+         }
+       }
     };
     callSendAPI(sender_psid, noPayload);
   } else if (payload === GREETING) {
