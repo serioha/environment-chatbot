90369433827a2395e12ad51298dea327e8f15f9b
Vivian Chan <viviancpy@gmail.com>
2017-12-29 00:24:08 +1100

    add first question postbacks

--- index.js
+++ index.js
@@ -30,11 +30,11 @@ app.post('/webhook', (req, res) => {
 
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
-      if (webhook_event.message) {
-        handleMessage(sender_psid, webhook_event.message);
-      } else if (webhook_event.postback) {
+      if (webhook_event.postback) {
         handlePostback(sender_psid, webhook_event.postback);
-      }
+      } else if (webhook_event.message) {
+        handleMessage(sender_psid, webhook_event.message);
+      } 
     });
     // Return a '200 OK' response to all events
     res.status(200).send('EVENT_RECEIVED');
