21a7cf75da2cc88e9244e8ebdc958239bc299cb4
Vivian Chan <viviancpy@gmail.com>
2017-12-29 18:57:35 +1100

    add location

--- index.js
+++ index.js
@@ -118,27 +118,64 @@ function handleMessage(sender_psid, message) {
       } else if (affected){
         response = {
           "attachment": {
-            "type":"template",
-            "payload":{
-              "template_type":"button",
-              "text":"What are you mostly interested in doing??",
-              "buttons":[
+            "type": "template",
+            "payload": {
+              "template_type": "list",
+              "top_element_style": "compact",
+              "elements": [
                 {
+                  "title": "Classic T-Shirt Collection",
+                  "subtitle": "See all our colors",
+                  "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",
+                  "buttons": [
+                    {
+                      "title": "View",
+                      "type": "web_url",
+                      "url": "https://peterssendreceiveapp.ngrok.io/collection",
+                      "messenger_extensions": true,
+                      "webview_height_ratio": "tall",
+                      "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                    }
+                  ]
+                },
+                {
+                  "title": "Classic White T-Shirt",
+                  "subtitle": "See all our colors",
+                  "default_action": {
+                    "type": "web_url",
+                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                    "messenger_extensions": false,
+                    "webview_height_ratio": "tall"
+                  }
+                },
+                {
+                  "title": "Classic Blue T-Shirt",
+                  "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+                  "subtitle": "100% Cotton, 200% Comfortable",
+                  "default_action": {
+                    "type": "web_url",
+                    "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
+                    "messenger_extensions": true,
+                    "webview_height_ratio": "tall",
+                    "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                  },
+                  "buttons": [
+                    {
+                      "title": "Shop Now",
+                      "type": "web_url",
+                      "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                      "messenger_extensions": true,
+                      "webview_height_ratio": "tall",
+                      "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                    }
+                  ]
+                }
+              ],
+               "buttons": [
+                {
+                  "title": "View More",
                   "type": "postback",
-                  "title": "Environmental Cleanups",
-                  "payload": PREF_CLEANUP
-                },{
-                  "type": "postback",
-                  "title": "Revegetation",
-                  "payload": PREF_REVEGETATION
-                },{
-                  "type": "postback",
-                  "title": "Bio surveys",
-                  "payload": PREF_BIO_SURVEY
-                },{
-                  "type": "postback",
-                  "title": "Canvassing",
-                  "payload": PREF_CANVASSING
+                  "payload": "payload"
                 }
               ]
             }
@@ -321,8 +358,6 @@ function callSendAPI(sender_psid, response) {
     "method": "POST",
     "json": request_body
   }, (err, res, body) => {
-    console.log('Send Messages Result err:', err);
-    console.log('Send Messages Result res:', res);
     if (err) {
       console.error("Unable to send message:" + err);
     }
