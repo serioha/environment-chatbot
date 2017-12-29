5294a46902b264cd8831ea2841d351beead6596d
Vivian Chan <viviancpy@gmail.com>
2017-12-30 00:43:18 +1100

    show orgs

--- index.js
+++ index.js
@@ -305,8 +305,8 @@ function handlePreferencePostback(sender_psid, chatStatus){
       if (err) {
         console.error("Unable to search Facebook API:" + err);
       } else {
-          console.log("Facebook API result:", body);
-          let elements = body["data"].map(org => {
+          console.log("Facebook API result:", res);
+          let elements = res.body.data.map(org => {
               let element = {
                 "title": org.name,
                 "buttons":[{
@@ -337,7 +337,7 @@ function handlePreferencePostback(sender_psid, chatStatus){
             }
           };
           callSendAPI(sender_psid, organizationPayload);
-      }
+        }
     });
   }
 }
