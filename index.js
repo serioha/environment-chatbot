0346eebd3ef4f77a32f48efefbb060deaad1ebf3
Sergey Ryzhkov <sergey.v.ryzhkov@gmail.com>
2020-06-27 00:37:39 +0300

    code

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.post('/webhook', (req, res) => {
 })
 
 // Accepts GET requests at the /webhook endpoint
-app.get('/webhook', (req, res) => {
+app.get('/webhook/', (req, res) => {
   /** UPDATE YOUR VERIFY TOKEN **/
   const VERIFY_TOKEN = process.env.VERIFICATION_TOKEN
 
